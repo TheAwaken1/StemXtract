f7be68bd0e4fe7a9f938d7130af17fd1ecfffb00
TheAwaken1 <cmpcreativedesigns@gmail.com>
2025-04-28 16:44:11 -0700

    Update start.js

--- start.js
+++ start.js
@@ -1,17 +1,20 @@
 module.exports = {
-  "daemon": true,
-  "run": [
+  daemon: true,
+  run: [
     {
-      "method": "shell.run",
-      "params": {
-        "venv": "env",
-        "path": "app",
-        "message": "python stemxtract.py", // Corrected script name
-        "on": [{
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        env: { },                   // Edit this to customize environment variables (see documentation)
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "python stemxtract.py",    // Edit with your custom commands
+        ],
+        on: [{
           // The regular expression pattern to monitor.
           // When this pattern occurs in the shell terminal, the shell will return,
           // and the script will go onto the next step.
-          "event": "/http:\\/\\/\\S+/",   
+          "event": "/http:\/\/\\S+/",   
 
           // "done": true will move to the next step while keeping the shell alive.
           // "kill": true will move to the next step after killing the shell.
@@ -20,15 +23,12 @@ module.exports = {
       }
     },
     {
-      "method": "local.set",
-      "params": {
-        "url": "{{input.event[0]}}"
-      }
-    },
-    {
-      "method": "browser.open",
-      "params": {
-        "uri": "{{input.event[0]}}"
+      // This step sets the local variable 'url'.
+      // This local variable will be used in pinokio.js to display the "Open WebUI" tab when the value is set.
+      method: "local.set",
+      params: {
+        // the input.event is the regular expression match object from the previous step
+        url: "{{input.event[0]}}"
       }
     }
   ]
