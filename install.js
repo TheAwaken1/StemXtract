357d15e751ef78e445522acb431442d0ef48f480
TheAwaken1 <cmpcreativedesigns@gmail.com>
2025-04-28 17:29:09 -0700

    Update install.js

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/TheAwaken1/StemXtract.git app",
+          "git clone https://github.com/TheAwaken1/StemXtract.git"
         ]
       }
     },
